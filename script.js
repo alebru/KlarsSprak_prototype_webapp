2305681786aea3d250ce3d6ef7f5dab148c284e2
Alexander Brusmark <brusmark@gmail.com>
2015-11-27 23:43:55 +0100

    Add scenario page, fix maintain full screen mode page-to-page

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 /**
  * Created by Alexander on 2015-11-27.
  */
+
+$(document).ready(function() {
+
+
+    function fullscreen() {
+        $('a').click(function() {
+            if(!$(this).hasClass('noeffect')) {
+                window.location = $(this).attr('href');
+                return false;
+            }
+        });
+    }
+
+    fullscreen();
+});
