4dc8c7d720839789d9ce16c15e31a3a48847cb90
Alexander Brusmark <brusmark@gmail.com>
2015-11-29 00:15:53 +0100

    Add scenarios page, horizontal scroll, toast dialogues

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ $(document).ready(function() {
     }
 
     fullscreen();
-    
-/*    document.body.addEventListener('touchmove', function(event) {
+
+/*
+    document.body.addEventListener('touchmove', function(event) {
         console.log(event.source);
         //if (event.source == document.body)
         event.preventDefault();
@@ -28,7 +29,8 @@ $(document).ready(function() {
 
     $(function() {
         window.onresize();
-    });*/
+    });
+*/
 
     $('select').material_select();
 });
