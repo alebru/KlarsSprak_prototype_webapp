1ece3f0f189f870ea040d7c2d85c6351d4674415
Alexander Brusmark <brusmark@gmail.com>
2015-11-28 01:21:42 +0100

    Add icons, remove scroll, placeholder page

--- script.js
+++ script.js
@@ -15,4 +15,18 @@ $(document).ready(function() {
     }
 
     fullscreen();
+
+    document.body.addEventListener('touchmove', function(event) {
+        console.log(event.source);
+        //if (event.source == document.body)
+        event.preventDefault();
+    }, false);
+
+    window.onresize = function() {
+        $(document.body).width(window.innerWidth).height(window.innerHeight);
+    }
+
+    $(function() {
+        window.onresize();
+    });
 });
