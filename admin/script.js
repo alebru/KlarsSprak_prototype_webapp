32b3eda7b7f1b1dfbe59435236ec872c2e1574b0
Alexander Brusmark <brusmark@gmail.com>
2015-11-30 19:25:29 +0100

    Added basic admin GUI

---
+++ admin/script.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Alexander on 2015-11-27.
+ */
+
+$(document).ready(function() {
+
+
+    function fullscreen() {
+        $('a').click(function() {
+            if(!$(this).hasClass('noeffect')) {
+                window.location = $(this).attr('href');
+                return false;
+            }
+        });
+    }
+
+    fullscreen();
+
+/*
+    document.body.addEventListener('touchmove', function(event) {
+        console.log(event.source);
+        //if (event.source == document.body)
+        event.preventDefault();
+    }, false);
+
+    window.onresize = function() {
+        $(document.body).width(window.innerWidth).height(window.innerHeight);
+    }
+
+    $(function() {
+        window.onresize();
+    });
+*/
+
+    $('select').material_select();
+});
